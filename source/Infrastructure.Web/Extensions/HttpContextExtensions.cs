0bd03c4b64c6d62072fbaf9b0fb8c3785eafe11a
AlexanderByndyu <alexander.byndyu@gmail.com>
2012-08-24 16:26:15 +0600

    Add HttpContextExtensions

---
+++ source/Infrastructure.Web/Extensions/HttpContextExtensions.cs
@@ -0,0 +1,17 @@
+﻿namespace ByndyuSoft.Infrastructure.Web.Extensions
+{
+	using System.Web;
+
+	public static class HttpContextExtensions
+	{
+		/// <summary>
+		/// Проверка аутентифицирован пользователь
+		/// </summary>
+		/// <param name="context"></param>
+		/// <returns></returns>
+		public static bool IsAuthenticated(this HttpContextBase context)
+		{
+			return context.User != null && context.User.Identity.IsAuthenticated;
+		}
+	}
+}
