88e38b1dc785199a4f4a9afae1b14942198c9c82
AlexanderByndyu <alexander.byndyu@gmail.com>
2012-10-19 18:11:39 +0600

    create SelectList from Enum

---
+++ source/Infrastructure.Web.Tests/Extensions/EnumExtensionsTests.cs
@@ -0,0 +1,25 @@
+﻿namespace Infrastructure.Web.Tests.Extensions
+{
+	using System.Linq;
+	using ByndyuSoft.Infrastructure.Web.Extensions;
+	using Xunit;
+
+	public class EnumExtensionsTests
+	{
+		enum TestEnum
+		{
+			First,
+			Second
+		}
+
+		[Fact]
+		public void CreateSelectListFromEnum()
+		{
+			var selectedList = EnumExtensions.ToSelectedList<TestEnum>();
+
+			var itesm = selectedList.Items.Cast<object>();
+
+			Assert.Equal(2, itesm.Count());
+		}
+	}
+}
---
+++ source/Infrastructure.Web/Extensions/EnumExtensions.cs
@@ -0,0 +1,13 @@
+﻿namespace ByndyuSoft.Infrastructure.Web.Extensions
+{
+	using System;
+	using System.Web.Mvc;
+
+	public static class EnumExtensions
+	{
+		public static SelectList ToSelectedList<TEnum>() where TEnum : struct, IConvertible
+		{
+			return new SelectList(Common.Extensions.EnumExtensions.ToKeyValuePairs<TEnum>(), "Key", "Value");
+		}
+	}
+}
