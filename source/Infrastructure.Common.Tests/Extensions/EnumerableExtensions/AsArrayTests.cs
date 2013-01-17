3041c3673be7b9e8b8280be7d26c35756ef849f1
AlexanderByndyu <alexander.byndyu@gmail.com>
2013-01-17 11:57:45 +0600

    add IEnumerable extension AsArray()

---
+++ source/Infrastructure.Common.Tests/Extensions/EnumerableExtensions/AsArrayTests.cs
@@ -0,0 +1,39 @@
+﻿namespace Infrastructure.Common.Tests.Extensions.EnumerableExtensions
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using ByndyuSoft.Infrastructure.Common.Extensions;
+    using Xunit;
+
+    public class AsArrayTests
+    {
+        [Fact]
+        public void ConvertListToArray()
+        {
+            IEnumerable<int> enumerable = new List<int> {1, 2};
+
+            int[] result = enumerable.AsArray();
+
+            Assert.Equal(2, result.Length);
+        }
+
+        [Fact]
+        public void IfPassNullThenReturnEmptyArray()
+        {
+            int[] result = ((IEnumerable<int>) null).AsArray();
+
+            Assert.Equal(0, result.Length);
+        }
+
+        [Fact]
+        public void ConvertIEnumerableToArray()
+        {
+            var list = new List<int> {1, 2};
+            IEnumerable<int> enumerable = list.Where(x => x > 0).AsEnumerable();
+            
+            int[] result = enumerable.AsArray();
+
+            Assert.Equal(2, result.Length);
+        }
+    }
+}
--- source/Infrastructure.Common/Extensions/EnumerableExtensions.cs
+++ source/Infrastructure.Common/Extensions/EnumerableExtensions.cs
@@ -2,11 +2,17 @@
 {
 	using System.Collections.Generic;
 	using System.Linq;
+    using JetBrains.Annotations;
 
+    /// <summary>
+    /// Методы расширения для <see cref="IEnumerable{T}"/>
+    /// </summary>
+    [UsedImplicitly]
 	public static class EnumerableExtensions
 	{
 		public static IEnumerable<T> Prepend<T>(this IEnumerable<T> enumerable, T item)
 		{
+            // TODO: add test and XML-comments
 			var tmp = new List<T> {item};
 			tmp.AddRange(enumerable.Select(x => x));
 			return tmp;
@@ -14,17 +20,33 @@
 
 		public static ICollection<T> ToHashSet<T>(this IEnumerable<T> enumerable)
 		{
+            // TODO: add test and XML-comments
 			return new HashSet<T>(enumerable);
 		}
 
 		public static bool IsEmpty<T>(this IEnumerable<T> enumerable)
 		{
+            // TODO: add test and XML-comments
 			return enumerable == null || enumerable.Any() == false;
 		}
 
 		public static bool IsNotEmpty<T>(this IEnumerable<T> enumerable)
 		{
+            // TODO: add test and XML-comments
 			return enumerable != null && enumerable.Any();
 		}
+
+        /// <summary>
+        /// Преобразовать перечисление в массив
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="source"></param>
+        [NotNull]
+        public static T[] AsArray<T>([CanBeNull] this IEnumerable<T> source)
+        {
+            return source != null
+                ? (source as T[] ?? source.ToArray())
+                : new T[0];
+        }
 	}
 }
