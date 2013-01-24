4b006e53bfd2a1ca5012ead0728ba05ecdcbc589
eskat0n <muyou.prj@gmail.com>
2013-01-24 09:33:08 +0600

    added LinqQueryBase generic class with two generic arguments

---
+++ source/Infrastructure.NHibernate/LinqQueryBase`2.cs
@@ -0,0 +1,35 @@
+﻿namespace ByndyuSoft.Infrastructure.NHibernate
+{
+    using System.Linq;
+    using Domain;
+    using JetBrains.Annotations;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <typeparam name="TCriterion"></typeparam>
+    /// <typeparam name="TResult"></typeparam>
+    [PublicAPI]
+    public abstract class LinqQueryBase<TCriterion, TResult> : IQuery<TCriterion, TResult>
+        where TCriterion : ICriterion
+    {
+        private readonly ILinqProvider _linq;
+
+        protected LinqQueryBase(ILinqProvider linq)
+        {
+            _linq = linq;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [PublicAPI]
+        public virtual IQueryable<TEntity> Query<TEntity>()
+            where TEntity : class, IEntity, new()
+        {
+            return _linq.Query<TEntity>();
+        }
+
+        public abstract TResult Ask(TCriterion criterion);
+    }
+}
