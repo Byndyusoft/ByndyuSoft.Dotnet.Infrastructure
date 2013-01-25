6974ad1b15cdecfb493b22858e1374c5f9ea2a73
eskat0n <muyou.prj@gmail.com>
2013-01-25 17:18:03 +0600

    added extension method for IQueryBuilder which allows to write queries more "fluently"

---
+++ source/Infrastructure.Domain/Extensions/QueryBuilderExtensions.cs
@@ -0,0 +1,27 @@
+﻿namespace ByndyuSoft.Infrastructure.Domain.Extensions
+{
+    using JetBrains.Annotations;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    [PublicAPI]
+    public static class QueryBuilderExtensions
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="queryBuilder"></param>
+        /// <param name="criterion"></param>
+        /// <typeparam name="TCriterion"></typeparam>
+        /// <returns></returns>
+        [PublicAPI]
+        public static int Count<TCriterion>(this IQueryBuilder queryBuilder, TCriterion criterion)
+            where TCriterion : class, ICriterion
+        {
+            return queryBuilder
+                .For<int>()
+                .With(criterion);
+        }
+    }
+}
