77a335b9ff631a34619c3473a00bbdab698c3a63
eskat0n <muyou.prj@gmail.com>
2013-01-25 17:04:55 +0600

    added DistinctBy extension method

--- source/Infrastructure.Common/Extensions/EnumerableExtensions.cs
+++ source/Infrastructure.Common/Extensions/EnumerableExtensions.cs
@@ -1,6 +1,7 @@
 ﻿namespace ByndyuSoft.Infrastructure.Common.Extensions
 {
-	using System.Collections.Generic;
+    using System;
+    using System.Collections.Generic;
 	using System.Linq;
     using JetBrains.Annotations;
 
@@ -10,29 +11,54 @@
     [UsedImplicitly]
 	public static class EnumerableExtensions
 	{
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="enumerable"></param>
+		/// <param name="item"></param>
+		/// <typeparam name="T"></typeparam>
+		/// <returns></returns>
 		public static IEnumerable<T> Prepend<T>(this IEnumerable<T> enumerable, T item)
 		{
-            // TODO: add test and XML-comments
+            // TODO: add tests and XMLdoc-comments
 			var tmp = new List<T> {item};
 			tmp.AddRange(enumerable.Select(x => x));
 			return tmp;
 		}
 
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="enumerable"></param>
+		/// <typeparam name="T"></typeparam>
+		/// <returns></returns>
 		public static ICollection<T> ToHashSet<T>(this IEnumerable<T> enumerable)
 		{
-            // TODO: add test and XML-comments
+            // TODO: add tests and XMLdoc-comments
 			return new HashSet<T>(enumerable);
 		}
 
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="enumerable"></param>
+		/// <typeparam name="T"></typeparam>
+		/// <returns></returns>
 		public static bool IsEmpty<T>(this IEnumerable<T> enumerable)
 		{
-            // TODO: add test and XML-comments
+            // TODO: add tests and XMLdoc-comments
 			return enumerable == null || enumerable.Any() == false;
 		}
 
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="enumerable"></param>
+		/// <typeparam name="T"></typeparam>
+		/// <returns></returns>
 		public static bool IsNotEmpty<T>(this IEnumerable<T> enumerable)
 		{
-            // TODO: add test and XML-comments
+            // TODO: add tests and XMLdoc-comments
 			return enumerable != null && enumerable.Any();
 		}
 
@@ -47,6 +73,40 @@
             return source != null
                 ? (source as T[] ?? source.ToArray())
                 : new T[0];
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="enumerable"></param>
+        /// <param name="accessor"></param>
+        /// <typeparam name="TSource"></typeparam>
+        /// <returns></returns>
+        public static IEnumerable<TSource> DistinctBy<TSource>(this IEnumerable<TSource> enumerable, Func<TSource, object> accessor)
+        {
+            // TODO: add tests and XMLdoc-comments
+            return enumerable
+                .Distinct(new PropertyEqualityComparer<TSource>(accessor));
+        }
+
+        private class PropertyEqualityComparer<TSource> : IEqualityComparer<TSource>
+        {
+            private readonly Func<TSource, object> _accessor;
+
+            public PropertyEqualityComparer(Func<TSource, object> accessor)
+            {
+                _accessor = accessor;
+            }
+
+            public bool Equals(TSource x, TSource y)
+            {
+                return _accessor(x).Equals(_accessor(y));
+            }
+
+            public int GetHashCode(TSource obj)
+            {
+                return _accessor(obj).GetHashCode();
+            }
         }
 	}
 }
