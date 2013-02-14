e71125f65ea7f909cf94002de11b10a2459f12ac
eskat0n <muyou.prj@gmail.com>
2013-02-14 14:55:24 +0600

    Add Contains() and LikewiseContains() to StringExtensions

--- source/Infrastructure.Common/Extensions/StringExtensions.cs
+++ source/Infrastructure.Common/Extensions/StringExtensions.cs
@@ -1,12 +1,15 @@
 ﻿namespace ByndyuSoft.Infrastructure.Common.Extensions
 {
-	using System.Collections.Generic;
+    using System;
+    using System.Collections.Generic;
 	using System.Collections.Specialized;
 	using System.Linq;
+    using JetBrains.Annotations;
 
 	///<summary>
 	/// Класс свойств расширений для коллекций строк
 	///</summary>
+	[PublicAPI]
 	public static class StringExtensions
 	{
 		///<summary>
@@ -15,6 +18,7 @@
 		///<param name="source"></param>
 		///<param name="separator"></param>
 		///<returns></returns>
+        [PublicAPI]
 		public static string Join(this IEnumerable<string> source, string separator)
 		{
 			return string.Join(separator, source);
@@ -26,9 +30,38 @@
 		///<param name="source"></param>
 		///<param name="separator"></param>
 		///<returns></returns>
+        [PublicAPI]
 		public static string Join(this StringCollection source, string separator)
 		{
 			return string.Join(separator, source.Cast<string>());
 		}
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="input"></param>
+        /// <param name="value"></param>
+        /// <param name="comparisonType"></param>
+        /// <returns></returns>
+        [PublicAPI]
+        public static bool Contains(this string input, string value, StringComparison comparisonType)
+        {
+            if (string.IsNullOrEmpty(input) == false)
+                return input.IndexOf(value, comparisonType) != -1;
+
+            return false;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="input"></param>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        [PublicAPI]
+        public static bool LikewiseContains(this string input, string value)
+        {
+            return Contains(input, value, StringComparison.InvariantCultureIgnoreCase);
+        }
 	}
 }
