58c68a76d3f8b0e5ba076545517a9586df5d52a8
eskat0n <muyou.prj@gmail.com>
2013-02-14 14:15:38 +0600

    Added default AutomappingConfiguration class

---
+++ source/Infrastructure.NHibernate/AutomappingConfiguration.cs
@@ -0,0 +1,34 @@
+﻿namespace ByndyuSoft.Infrastructure.NHibernate
+{
+    using System;
+    using Domain;
+    using FluentNHibernate.Automapping;
+    using JetBrains.Annotations;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    [PublicAPI]
+    public class AutomappingConfiguration : DefaultAutomappingConfiguration
+    {
+        public override bool AbstractClassIsLayerSupertype(Type type)
+        {
+            return true;
+        }
+
+        public override bool IsComponent(Type type)
+        {
+            return typeof (IEntityComponent).IsAssignableFrom(type);
+        }
+
+        public override bool IsDiscriminated(Type type)
+        {
+            return true;
+        }
+
+        public override bool ShouldMap(Type type)
+        {
+            return typeof (IEntity).IsAssignableFrom(type);
+        }
+    }
+}
