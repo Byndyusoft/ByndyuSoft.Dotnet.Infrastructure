55a7a2ea8879d753c77004e801a2407e6629f9e2
Eugene Sergueev <sergueeveugene@hotmail.com>
2013-03-14 16:26:42 +0300

    Add base class Disposable which holds reference of one or more resource object

---
+++ source/Infrastructure.Common/Disposable.cs
@@ -0,0 +1,52 @@
+﻿namespace ByndyuSoft.Infrastructure.Common
+{
+    using System;
+    using System.Diagnostics;
+
+    /// <summary>
+    /// Defines a base class which holds reference of one or more resource object.
+    /// </summary>
+    public abstract class Disposable : IDisposable
+    {
+        private bool disposed;
+
+        /// <summary>
+        /// Releases unmanaged resources and performs other cleanup operations before the
+        /// <see cref="Disposable"/> is reclaimed by garbage collection.
+        /// </summary>
+        [DebuggerStepThrough]
+        ~Disposable()
+        {
+            Dispose(false);
+        }
+
+        /// <summary>
+        /// Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
+        /// </summary>
+        [DebuggerStepThrough]
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        /// <summary>
+        /// Disposes the resources.
+        /// </summary>
+        [DebuggerStepThrough]
+        protected virtual void DisposeCore()
+        {
+        }
+
+        [DebuggerStepThrough]
+        private void Dispose(bool disposing)
+        {
+            if (!disposed && disposing)
+            {
+                DisposeCore();
+            }
+
+            disposed = true;
+        }
+    }
+}
