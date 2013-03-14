51bb6e9af30ac49be339ea819bb873d8e33c116b
Eugene Sergueev <sergueeveugene@hotmail.com>
2013-03-14 16:33:05 +0300

    Add IConnectionProvider interface and default implementation DefaultDapperConnectionProvider

---
+++ source/Infrastructure.Dapper/DefaultDapperConnectionProvider.cs
@@ -0,0 +1,68 @@
+﻿namespace ByndyuSoft.Infrastructure.Dapper
+{
+    using System;
+    using System.Data;
+
+    using ByndyuSoft.Infrastructure.Common;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public class DefaultDapperConnectionProvider : Disposable, IConnectionProvider
+    {
+        private readonly IConnectionFactory connectionFactory;
+        private IDbConnection connection;
+        private IDbTransaction transaction;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="connectionFactory"></param>
+        /// <exception cref="ArgumentNullException"></exception>
+        public DefaultDapperConnectionProvider(IConnectionFactory connectionFactory)
+        {
+            if (connectionFactory == null)
+                throw new ArgumentNullException("connectionFactory");
+
+            this.connectionFactory = connectionFactory;
+        }
+
+        public IDbConnection CurrentConnection
+        {
+            get
+            {
+                if (connection != null)
+                    return connection;
+
+                connection = connectionFactory.Create();
+                transaction = connection.BeginTransaction();
+
+                return connection;
+            }
+        }
+
+        protected override void DisposeCore()
+        {
+            if (connection == null)
+                return;
+
+            try
+            {
+                transaction.Commit();
+            }
+            catch
+            {
+                transaction.Rollback();
+                throw;
+            }
+            finally
+            {
+                transaction.Dispose();
+            }
+
+            connection.Dispose();
+            connection = null;
+            transaction = null;
+        }
+    }
+}
---
+++ source/Infrastructure.Dapper/IConnectionProvider.cs
@@ -0,0 +1,15 @@
+﻿namespace ByndyuSoft.Infrastructure.Dapper
+{
+    using System.Data;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public interface IConnectionProvider
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        IDbConnection CurrentConnection { get; }
+    }
+}
