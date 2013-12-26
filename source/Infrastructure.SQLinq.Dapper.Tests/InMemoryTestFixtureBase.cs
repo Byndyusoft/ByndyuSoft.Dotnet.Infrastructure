6524abfe4339b5646086189fd8b846acd142dfd7
Alexander <sania.kill@mail.ru>
2013-12-26 15:57:35 +0600

    Добавил проект с тестами для SQLinq.Dapper

---
+++ source/Infrastructure.SQLinq.Dapper.Tests/InMemoryTestFixtureBase.cs
@@ -0,0 +1,50 @@
+﻿namespace Infrastructure.SQLinq.Dapper.Tests
+{
+    using System.Data;
+    using ByndyuSoft.Infrastructure.Dapper;
+    using Dto;
+    using NUnit.Framework;
+    using QueryObject;
+
+    public class InMemoryTestFixtureBase
+    {
+        private readonly ProductDto _product = new ProductDto();
+
+        protected const string FirstProductName = "Product #1";
+        protected const string SecondProductName = "Product #2";
+        protected const string ThirdProductName = "Product #3";
+
+        [SetUp]
+        public void Setup()
+        {
+            using (IDbConnection dbConnection = new SqliteConnectionFactory().Create())
+            {
+                var createProduct = new CreateProductTable();
+                dbConnection.Execute(createProduct.Query());
+
+                InsertProductWithName(dbConnection, FirstProductName);
+
+                InsertProductWithName(dbConnection, SecondProductName);
+
+                InsertProductWithName(dbConnection, ThirdProductName);
+            }
+        }
+
+        protected void InsertProductWithName(IDbConnection dbConnection, string newName)
+        {
+            _product.Name = newName;
+
+            dbConnection.Execute(new InsertProduct().Query(_product));
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            using (IDbConnection dbConnection = new SqliteConnectionFactory().Create())
+            {
+                var dropProductTable = new DropProductTable();
+                dbConnection.Execute(dropProductTable.Query());
+            }
+        }
+    }
+}
---
+++ source/Infrastructure.SQLinq.Dapper.Tests/QueryObject/CreateProductTable.cs
@@ -0,0 +1,14 @@
+﻿namespace Infrastructure.SQLinq.Dapper.Tests.QueryObject
+{
+    using ByndyuSoft.Infrastructure.Dapper;
+
+    public class CreateProductTable
+    {
+        public QueryObject Query()
+        {
+            return new QueryObject(@"CREATE TABLE Product (
+                                    Product_Id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                    Name varchar(255) NOT NULL)");
+        }
+    }
+}
