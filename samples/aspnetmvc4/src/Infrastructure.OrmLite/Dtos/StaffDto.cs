f73eb08a3bac52b03385068f3dde7bd996991b7a
Alexander <sania.kill@mail.ru>
2014-01-17 19:00:56 +0600

    Реализовал добавление посоха

---
+++ samples/aspnetmvc4/src/Infrastructure.OrmLite/Dtos/StaffDto.cs
@@ -0,0 +1,17 @@
+﻿namespace Mvc4Sample.Infrastructure.OrmLite.Dtos
+{
+    using System;
+    using ServiceStack.DataAnnotations;
+
+    [Alias("STAFF")]
+    public class StaffDto
+    {
+        public int STAFF_ID { get; set; }
+
+        public string NAME { get; set; }
+
+        public int QUANTITY { get; set; }
+
+        public DateTime CREATED_AT { get; set; }
+    }
+}
---
+++ samples/aspnetmvc4/src/Infrastructure.OrmLite/Staff/Commands/CreateStaffCommand.cs
@@ -0,0 +1,24 @@
+﻿namespace Mvc4Sample.Infrastructure.OrmLite.Staff.Commands
+{
+    using ByndyuSoft.Infrastructure.Domain.Commands;
+    using Domain.Entities;
+    using Dtos;
+    using ServiceStack.OrmLite;
+    using ServiceStack.OrmLite.SqlServer;
+    using Web.Application.Staff.Commands.Contexts;
+
+    public class CreateStaffCommand : ICommand<CreateStaffCommandContext>
+    {
+        public void Execute(CreateStaffCommandContext commandContext)
+        {
+            OrmLiteConfig.DialectProvider = new SqlServerOrmLiteDialectProvider();
+
+            using (var connection = ConnectionFactory.Open())
+            {
+                var staff = new Staff(commandContext.Form.Name, commandContext.Form.Quantity);
+
+                connection.InsertOnly(new StaffDto {NAME = staff.Name, QUANTITY = staff.Quantity, CREATED_AT = staff.CreatedAt}, ev => ev.Insert(p => new {p.NAME, p.QUANTITY, p.CREATED_AT}));
+            }
+        }
+    }
+}
