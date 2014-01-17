9faa1edb7d8355a2ceeb3d6b647e79bc21685768
Alexander <sania.kill@mail.ru>
2014-01-17 19:01:29 +0600

    реализовал пустую выдачу по пейджингу для OrmLite

---
+++ samples/aspnetmvc4/src/Infrastructure.OrmLite/Staff/Queries/FindStaffListViewModelForPageQuery.cs
@@ -0,0 +1,20 @@
+﻿namespace Mvc4Sample.Infrastructure.OrmLite.Staff.Queries
+{
+    using System.Linq;
+    using ByndyuSoft.Infrastructure.Domain;
+    using ByndyuSoft.Infrastructure.Web.Mvc.Paging;
+    using Web.Application.Staff.Criteria;
+    using Web.Application.Staff.ViewModels;
+
+    public class FindStaffListViewModelForPageQuery : IQuery<FindStaffListViewModelForPage, StaffListViewModel>
+    {
+        public StaffListViewModel Ask(FindStaffListViewModelForPage criterion)
+        {
+            return new StaffListViewModel
+                {
+                    Staff = Enumerable.Empty<StaffViewModel>(),
+                    PageInfo = new PageInfo(criterion.Page - 1, criterion.PageSize, 0)
+                };
+        }
+    }
+}
