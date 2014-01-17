90b954461b95e9f65ef1675a70d19db30c78204a
Alexander <sania.kill@mail.ru>
2014-01-17 19:01:08 +0600

    Реализовал поиск пользователя по логину

---
+++ samples/aspnetmvc4/src/Infrastructure.OrmLite/User/Queries/FindByLoginQuery.cs
@@ -0,0 +1,34 @@
+﻿namespace Mvc4Sample.Infrastructure.OrmLite.User.Queries
+{
+    using System;
+    using System.Linq;
+    using ByndyuSoft.Infrastructure.Domain;
+    using Domain.Entities;
+    using Dtos;
+    using ServiceStack.OrmLite;
+    using ServiceStack.OrmLite.SqlServer;
+    using Web.Application.Account.Criteria;
+
+    public class FindByLoginQuery : IQuery<FindByLogin, User>
+    {
+        public User Ask(FindByLogin criterion)
+        {
+            OrmLiteConfig.DialectProvider = new SqlServerOrmLiteDialectProvider();
+
+            using (var connection = ConnectionFactory.Open())
+            {
+                var userDto = connection.Select<UserDto>(x => x.LOGIN == criterion.Login)
+                                        .Single();
+
+                var result = new User(userDto.NAME, userDto.EMAIL, userDto.LOGIN)
+                    {
+                        Id = userDto.USER_ID
+                    };
+
+                result.SetPassword(new Password(userDto.PASSWORD_HASH, userDto.PASSWORD_SALT));
+
+                return result;
+            }
+        }
+    }
+}
