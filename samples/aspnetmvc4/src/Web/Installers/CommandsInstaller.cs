f67039a0a2fea3e81a7286da5d8d30c46ac33b95
Alexander <sania.kill@mail.ru>
2014-01-20 10:06:42 +0600

    Вынес инсталлер для команд, повидимому инсталлеры выполняются по алфавиту, поэтому вынес container.AddFacility<TypedFactoryFacility>(); в CommandsInstaller

--- samples/aspnetmvc4/src/Web/Installers/CommandsInstaller.cs
+++ samples/aspnetmvc4/src/Web/Installers/CommandsInstaller.cs
@@ -1,32 +1,23 @@
 ﻿namespace Web.Installers
 {
     using ByndyuSoft.Infrastructure.Domain.Commands;
-    using ByndyuSoft.Infrastructure.Web.Forms;
     using Castle.Facilities.TypedFactory;
     using Castle.MicroKernel.Registration;
     using Castle.MicroKernel.SubSystems.Configuration;
     using Castle.Windsor;
     using Mvc4Sample.Infrastructure.OrmLite.Dtos;
 
-    public class FormHandlerAndCommandsInstaller : IWindsorInstaller
+    public class CommandsInstaller : IWindsorInstaller
     {
         public void Install(IWindsorContainer container, IConfigurationStore store)
         {
             container.AddFacility<TypedFactoryFacility>();
 
-            var formHandlers = Types.FromAssemblyNamed("Mvc4Sample.Web.Application")
-                                    .BasedOn(typeof (IFormHandler<>))
-                                    .WithService.AllInterfaces()
-                                    .LifestyleTransient();
-
-            container.Register(formHandlers,
-                               Component.For<IFormHandlerFactory>().AsFactory().LifestyleTransient());
-
             var commands = Types.FromAssemblyContaining<UserDto>()
-                    .BasedOn(typeof(ICommand<>))
-                    .WithService
-                    .AllInterfaces()
-                    .LifestyleTransient();
+                                .BasedOn(typeof (ICommand<>))
+                                .WithService
+                                .AllInterfaces()
+                                .LifestyleTransient();
 
             container.Register(commands,
                                Component.For<ICommandBuilder>().ImplementedBy<CommandBuilder>().LifestyleTransient(),
---
+++ samples/aspnetmvc4/src/Web/Installers/FormHandlersInstaller.cs
@@ -0,0 +1,23 @@
+﻿namespace Web.Installers
+{
+    using ByndyuSoft.Infrastructure.Web.Forms;
+    using Castle.Facilities.TypedFactory;
+    using Castle.MicroKernel.Registration;
+    using Castle.MicroKernel.SubSystems.Configuration;
+    using Castle.Windsor;
+    using Mvc4Sample.Web.Application.Account.Forms.Handlers;
+
+    public class FormHandlersInstaller : IWindsorInstaller
+    {
+        public void Install(IWindsorContainer container, IConfigurationStore store)
+        {
+            var formHandlers = Types.FromAssemblyContaining<SignInHandler>()
+                                    .BasedOn(typeof (IFormHandler<>))
+                                    .WithService.AllInterfaces()
+                                    .LifestyleTransient();
+
+            container.Register(formHandlers,
+                               Component.For<IFormHandlerFactory>().AsFactory().LifestyleTransient());
+        }
+    }
+}
