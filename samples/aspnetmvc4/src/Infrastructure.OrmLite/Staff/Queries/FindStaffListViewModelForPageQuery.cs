c66733ba51eec19cf7fc9239638fa4e328f798fd
Alexander <sania.kill@mail.ru>
2014-01-20 09:34:43 +0600

    Реализовал пейджинг по Staff через OrmLite

--- samples/aspnetmvc4/src/Infrastructure.OrmLite/Staff/Queries/FindStaffListViewModelForPageQuery.cs
+++ samples/aspnetmvc4/src/Infrastructure.OrmLite/Staff/Queries/FindStaffListViewModelForPageQuery.cs
@@ -3,6 +3,9 @@
     using System.Linq;
     using ByndyuSoft.Infrastructure.Domain;
     using ByndyuSoft.Infrastructure.Web.Mvc.Paging;
+    using Dtos;
+    using ServiceStack.OrmLite;
+    using ServiceStack.OrmLite.SqlServer;
     using Web.Application.Staff.Criteria;
     using Web.Application.Staff.ViewModels;
 
@@ -10,11 +13,29 @@
     {
         public StaffListViewModel Ask(FindStaffListViewModelForPage criterion)
         {
-            return new StaffListViewModel
-                {
-                    Staff = Enumerable.Empty<StaffViewModel>(),
-                    PageInfo = new PageInfo(criterion.Page - 1, criterion.PageSize, 0)
-                };
+            OrmLiteConfig.DialectProvider = new SqlServerOrmLiteDialectProvider();
+
+            using (var connection = ConnectionFactory.Open())
+            {
+                var totalCount = connection.Scalar<int>("SELECT COUNT(*) FROM STAFF");
+
+                var offset = (criterion.Page - 1)*criterion.PageSize;
+
+                var staffOnPage = connection.Select<StaffDto>("SELECT * FROM STAFF ORDER BY CREATED_AT DESC OFFSET @offset ROWS FETCH NEXT @pageSize ROWS ONLY", new { offset, pageSize = criterion.PageSize });
+
+                return new StaffListViewModel
+                    {
+                        Staff = staffOnPage
+                            .Select(x => new StaffViewModel
+                                {
+                                    Id = x.STAFF_ID,
+                                    Name = x.NAME,
+                                    Quantity = x.QUANTITY,
+                                    CreatedAt = x.CREATED_AT
+                                }),
+                        PageInfo = new PageInfo(criterion.Page - 1, criterion.PageSize, totalCount)
+                    };
+            }
         }
     }
 }
