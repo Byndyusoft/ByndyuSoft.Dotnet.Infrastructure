9cb73a866405b588ae007cb21016c15227cc3daa
victor.govorov <victorgovorov@mail.ru>
2016-09-05 00:21:44 +0500

    ported Infrastructure.Dapper project to .NET core

--- source/Infrastructure.Dapper/Properties/AssemblyInfo.cs
+++ source/Infrastructure.Dapper/Properties/AssemblyInfo.cs
@@ -1,5 +1,5 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("Infrastructure.Dapper")]
+[assembly: AssemblyTitle("ByndyuSoft.Infrastructure.Dapper")]
 [assembly: Guid("df75923c-870b-454d-a79a-e3e19b90a8b5")]
