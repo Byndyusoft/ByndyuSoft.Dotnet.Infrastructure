8c1081edce27b6412b7aaef4da86ee4a0730715f
Anton Nurgatin <nurgatin@byndyusoft.com>
2016-12-09 19:19:33 +0600

    добавил тест на валидность пароля

---
+++ samples/aspnetmvc/src/Domain.Model.Tests/PasswordTest.cs
@@ -0,0 +1,27 @@
+﻿﻿namespace MvcSample.Domain.Model.Tests
+ {
+     using Entities;
+     using NUnit.Framework;
+
+     [TestFixture]
+     public class PasswordTest
+     {
+         Password _password;
+
+         [SetUp]
+         public void SetUp()
+         {
+             _password = new Password("Password123456123456");
+         }
+
+         [TestCase("Password123456123456", Result = true)]
+         [TestCase("Password123456123456123456123456", Result = false)]
+         [TestCase("Password123123456456123123456456", Result = false)]
+         [TestCase("Password123456", Result = false)]
+         [TestCase("Password1123456", Result = false)]
+         public bool Check(string checkedPassword)
+         {
+             return _password.Check(checkedPassword);
+         }
+     }
+ }
