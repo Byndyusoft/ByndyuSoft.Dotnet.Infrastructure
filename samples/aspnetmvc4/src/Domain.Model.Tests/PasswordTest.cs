d6827bba92cf58154d6f6b2d226ee1a5535139bd
Anton Nurgatin <nurgatin@byndyusoft.com>
2016-12-12 17:38:08 +0600

    change sha512 to PBKDF2  and add test to check hash length

--- samples/aspnetmvc4/src/Domain.Model.Tests/PasswordTest.cs
+++ samples/aspnetmvc4/src/Domain.Model.Tests/PasswordTest.cs
@@ -1,19 +1,13 @@
 ﻿namespace Mvc4Sample.Domain.Model.Tests
 {
+    using System;
+    using System.Text;
     using Entities;
     using NUnit.Framework;
 
     [TestFixture]
     public class PasswordTest
     {
-        Password _password;
-
-        [SetUp]
-        public void SetUp()
-        {
-            _password = new Password("Password123456123456");
-        }
-
         [TestCase("Password123456123456", Result = true)]
         [TestCase("Password123456123456123456123456", Result = false)]
         [TestCase("Password123123456456123123456456", Result = false)]
@@ -21,7 +15,33 @@
         [TestCase("Password1123456", Result = false)]
         public bool Check(string checkedPassword)
         {
-            return _password.Check(checkedPassword);
+            Password password = new Password("Password123456123456");
+            return password.Check(checkedPassword);
+        }
+
+        [Test]
+        public void HashLengthShouldBeConst()
+        {
+            Password passwordSample = new Password("привет");
+            
+            for (int i = 1; i < 10000; i++)
+            {
+                Password checkedPassword = new Password(GetRandomString(i));
+                Assert.AreEqual(passwordSample.Hash.Length, checkedPassword.Hash.Length);
+            }
+        }
+
+        private static string GetRandomString(int length)
+        {
+            Random random = new Random();
+            StringBuilder result = new StringBuilder();
+
+            const string chars = "~!@#№$%^&*()\":{}'[];/?.,/\\*-+ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЬЫЪЭЮЯабвгдеёжзийклмнопрстуфхцчшщьыъэюя";
+
+            for (int i = 0; i < length; i++)
+                result.Append(chars[random.Next(chars.Length)]);
+
+            return result.ToString();
         }
     }
 }
